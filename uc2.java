5c82a4dcbed91c83e37ffed9b6fecdcaa81b6897
joshuva2009 <joshuvaanaveenarul@gmail.com>
2026-02-17 15:39:13 +0530

    Add files via upload

---
+++ uc2.java
@@ -0,0 +1,9 @@
+// Hint: Use the Example below as a guide to construct each line of the banner
+// Example for 05 which stands for Operating System:
+System.out.println(" *** *****");
+System.out.println("** ");
+System.out.println("** ");
+System.out.println("** *****");
+System.out.println("** **");
+System.out.println(" *** *****");
+System.out.println("** ** +""+"** **");
